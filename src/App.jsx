95e2080ec2cc24eb9f2f2bddac22c96849a59744
penso996 <penso.andrea@outlook.it>
2025-06-05 13:17:44 +0200

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 function App() {
 
+  const products = [
+    { id: 1, name: 'Mela', price: 0.5 },
+    { id: 2, name: 'Pane', price: 1.2 },
+    { id: 3, name: 'Latte', price: 1.0 },
+    { id: 4, name: 'Pasta', price: 0.7 },
+  ];
+
   return (
-    <>
-    </>
+    <div>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}, {product.price}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
