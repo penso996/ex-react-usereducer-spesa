e76d37aaba8718b7f210058e213219b03b11b278
penso996 <penso.andrea@outlook.it>
2025-06-05 13:46:32 +0200

    added working cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
 
   const products = [
@@ -7,15 +9,44 @@ function App() {
     { id: 4, name: 'Pasta', price: 0.7 },
   ];
 
+  const [addedProducts, setAddedProducts] = useState([]);
+  console.log(addedProducts)
+
+  const addToCart = product => {
+    const isAlreadyAdded = addedProducts.some(p => p.name === product.name);
+    if (isAlreadyAdded) {
+      return;
+    }
+    setAddedProducts(curr => [...curr, {
+      ...product,
+      quantity: 1
+    }]);
+  }
+
   return (
-    <div>
-      <h1>Prodotti</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name}, {product.price}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div>
+        <h1>Prodotti</h1>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.name}, {product.price.toFixed(2)}€
+              <button onClick={() => addToCart(product)}>Aggiungi al carrello</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        <h2>Prodotti nel Carrello</h2>
+        {addedProducts.length > 0 && (<ul>
+
+          {addedProducts.map((product) => (
+            <li key={product.id}>{product.name}, {product.price.toFixed(2)}€, {product.quantity} unità</li>
+          ))}
+
+        </ul>)}
+      </div>
+    </>
   )
 }
 
